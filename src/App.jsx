e62509e6e319485e63d018f5c428413234bdbc0a
Arfi Hussain <arfihussain27@gmail.com>
2025-04-11 23:18:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <div className="app-container min-h-screen bg-gray-200">
+    </div>
+  );
+}
+
+export default App;
